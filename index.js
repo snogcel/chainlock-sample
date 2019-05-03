856885aa6f2c80dc1b0918880d25854f61a431af
snogcel <ledniknoj@gmail.com>
2019-05-03 16:22:00 +0000

    integrate dashcore-lib

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let zmq = require('zeromq');
+const zmq = require('zeromq');
+const Dashcore = require('@dashevo/dashcore-lib');
 
-let socket = zmq.socket('sub');
+const socket = zmq.socket('sub');
 socket.connect('tcp://127.0.0.1:28332'); // connect to Dash Core daemon using the following configuration...
 
 /*
@@ -15,10 +16,38 @@ zmqpubrawblock=tcp://127.0.0.1:28332
  */
 
 socket.subscribe('rawblock'); // subscribe to "zmqpubrawblock"
+
 socket.subscribe('rawtx'); // subscribe to "zmqpubrawtx"
 
 console.log('Connected to daemon using port 28332');
 
 socket.on('message', function(topic, message) {
-    console.log('received a message related to:', topic.toString(), 'containing message:', message);
+
+    let zmqpub = topic.toString();
+
+    switch(zmqpub) {
+
+        case 'rawblock':
+
+            let block = new Dashcore.Block(message);
+
+            console.log('rawblock:', block.toJSON());
+
+            break;
+
+
+        case 'rawtx':
+
+            let tx = new Dashcore.Transaction(message);
+
+            console.log('rawtx:', tx.toJSON());
+
+            break;
+
+
+        default:
+
+            console.log("unknown message received.");
+    }
+
 });
